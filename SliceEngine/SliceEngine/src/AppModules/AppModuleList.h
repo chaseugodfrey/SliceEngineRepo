abc95b0eaaf98efb6b7cdd48338dfc77f0348e8b
chaseugodfrey <143150195+chaseugodfrey@users.noreply.github.com>
2025-03-14 23:31:35 +0800

    Reset

--- SliceEngine/SliceEngine/src/AppModules/AppModuleList.h
+++
@@ -1,7 +0,0 @@
-#ifndef SLICE_APP_MOPDULE_LIST_H
-#define SLICE_APP_MOPDULE_LIST_H
-
-#include "WindowManager.h"
-#include "SessionManager.h"
-
-#endif
--- SliceEngine/SliceEngine/src/AppModules/WindowManager.cpp
+++
@@ -1,2 +0,0 @@
-#include "../src/pch.h"
-#include "WindowManager.h"
