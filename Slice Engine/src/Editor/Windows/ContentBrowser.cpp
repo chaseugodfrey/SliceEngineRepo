778724bd528af9f7b2aa10af7d608edd8c50cc65
nic <nicklaiabc123@gmail.com>
2025-09-02 21:44:08 +0800

    The Start of the Content Browser
    Barely anything touched right now, just getting a feel back into ImGui again.

--- Slice Engine/src/Editor/Windows/ContentBrowser.cpp
+++ Slice Engine/src/Editor/Windows/ContentBrowser.cpp
@@ -7,6 +7,14 @@ namespace SliceEngine
 	{
 		ImGui::Begin("Content Browser");
 
+		ImGui::BeginChild("Hierarchy",ImVec2(200,150));
+		ImGui::Text("Hierarchy Here");
+		ImGui::EndChild();
+
+		ImGui::BeginChild("Current Folder", ImVec2(300,300));
+		ImGui::Text("Current Folder");
+		ImGui::EndChild();
+
 		ImGui::End();
 	}
 
