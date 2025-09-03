533d1c5c0e7095edb8e8dae153e8969d216f1f5b
nic <nicklaiabc123@gmail.com>
2025-09-03 21:56:22 +0800

    WIP Content Browser

--- Slice Engine/src/Editor/Windows/ContentBrowser.h
+++ Slice Engine/src/Editor/Windows/ContentBrowser.h
@@ -2,12 +2,16 @@
 #define CONTENT_BROWSER_H
 
 #include "../EditorWindow.h"
+#include "../EditorState.h"
 #include "../ContentBrowserState.h"
 
 namespace SliceEngine
 {
 	class ContentBrowser : public EditorWindow
 	{
+		//ContentBrowserState& contentBrowser;
+
+		//ContentBrowser(EditorState& editorState);
 	public:
 		void Draw() override final;
 	};
