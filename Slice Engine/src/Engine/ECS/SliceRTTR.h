b38977be844646f9e9e142a94d83e690abd212ee
Gideon Francis <gideonnfrancis999@gmail.com>
2025-09-07 05:00:15 +0800

    nvm rttr is broken still

---
+++ Slice Engine/src/Engine/ECS/SliceRTTR.h
@@ -0,0 +1,31 @@
+#include "../Graphics/WorldSpaceGraphicsSystem.h"
+#include "../TransformSystem.h"
+#include "../SoundSystem.h"
+#include <rttr/registration.h>
+
+namespace SliceEngine
+{
+	//RTTR_REGISTRATION
+	//{
+	//	// Register the interface for base system
+	//	rttr::registration::class_<IBaseSystem>("IBaseSystem")
+	//	.method("Bind", &IBaseSystem::Bind)
+	//	.method("Unbind", &IBaseSystem::Unbind);
+	//	rttr::registration::class_<WorldSpaceGraphicsSystem>("WorldSpaceGraphicsSystem")
+	//		.constructor<>()
+	//		.method("EntityOnEnter", &WorldSpaceGraphicsSystem::EntityOnEnter)
+	//		.method("EntityOnExit", &WorldSpaceGraphicsSystem::EntityOnExit)
+	//		.method("EntityOnUpdate", &WorldSpaceGraphicsSystem::EntityOnUpdate);
+	//	rttr::registration::class_<TransformSystem>("TransformSystem")
+	//		.constructor<>()
+	//		.method("EntityOnEnter", &TransformSystem::EntityOnEnter)
+	//		.method("EntityOnExit", &TransformSystem::EntityOnExit)
+	//		.method("EntityOnUpdate", &TransformSystem::EntityOnUpdate);
+	//	rttr::registration::class_<SoundSystem>("SoundSystem")
+	//		.constructor<>()
+	//		.method("EntityOnEnter", &SoundSystem::EntityOnEnter)
+	//		.method("EntityOnExit", &SoundSystem::EntityOnExit)
+	//		.method("EntityOnUpdate", &SoundSystem::EntityOnUpdate);
+	//}
+
+}
