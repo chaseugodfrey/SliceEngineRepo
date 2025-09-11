ee36592f02785caaa3b036519c22838dd0662f9b
Kalistee <144081400+Kalistee@users.noreply.github.com>
2025-09-11 08:59:12 +0800

    setup cont

---
+++ Slice Engine/src/Engine/FrameManager/FramerateManager.h
@@ -0,0 +1,21 @@
+#ifndef FRAMERATE_MANAGER_H
+#define FRAMERATE_MANAGER_H
+
+namespace SliceEngine
+{
+
+	class FramerateManager
+	{
+
+	public:
+
+		void Init();
+		void Update();
+		void Exit();
+
+	private:
+
+	};
+}
+
+#endif
